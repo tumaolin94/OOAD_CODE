e11964bc7077de6d721a30b655474214f708c8d9
Maolin Tu <tumaolin94@gmail.com>
2019-03-02 17:21:18 -0800

    update

--- src/chapter2/DogDoorSimulator.java
+++ src/chapter2/DogDoorSimulator.java
@@ -4,7 +4,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class DogDoorSimulator {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     DogDoor door = new DogDoor();
     Remote remote = new Remote(door);
 
@@ -14,16 +14,15 @@ public class DogDoorSimulator {
 //    remote.pressButton();
     System.out.println("Fido's all done...");
 //    remote.pressButton();
+    try {
+      Thread.currentThread().sleep(5000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    System.out.println("Fido barks to go inside...");
+    remote.pressButton();
     System.out.println("Fido's back inside...");
 //    remote.pressButton();
 
-    final Timer timer = new Timer();
-    timer.schedule(new TimerTask() {
-      @Override
-      public void run() {
-        remote.pressButton();
-        timer.cancel();
-      }
-    },5000);
   }
 }
